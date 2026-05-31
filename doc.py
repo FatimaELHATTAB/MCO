13ef5035b249e1740dc34e58656005df01578a72
ELHATTAB Fatima <32653132+FatimaELHATTAB@users.noreply.github.com>
2026-05-31 23:44:26 +0200

    Update doc.py

--- doc.py
+++ doc.py
@@ -1,4 +1,7 @@
+
 INSERT INTO own_91109_svg_um.provider_data_normalized
-SELECT * FROM own_91109_svg_um.provider_data_normalized_old
-ON CONFLICT (local_id, incorporation_country, flux_source) DO NOTHING
-LIMIT 100000;
+SELECT * FROM (
+    SELECT * FROM own_91109_svg_um.provider_data_normalized_old
+    LIMIT 100000
+) sub
+ON CONFLICT (local_id, incorporation_country, flux_source) DO NOTHING;
