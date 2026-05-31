bf04d1716177126d57d2c689853d1993d90237a6
ELHATTAB Fatima <32653132+FatimaELHATTAB@users.noreply.github.com>
2026-05-31 23:42:30 +0200

    Refactor SQL query for provider data insertion

--- doc.py
+++ doc.py
@@ -1,13 +1,4 @@
-\timing on
-
 INSERT INTO own_91109_svg_um.provider_data_normalized
-SELECT o.* 
-FROM own_91109_svg_um.provider_data_normalized_old o
-WHERE NOT EXISTS (
-    SELECT 1 
-    FROM own_91109_svg_um.provider_data_normalized n
-    WHERE n.local_id = o.local_id
-      AND n.incorporation_country = o.incorporation_country
-      AND n.flux_source = o.flux_source
-)
+SELECT * FROM own_91109_svg_um.provider_data_normalized_old
+ON CONFLICT (local_id, incorporation_country, flux_source) DO NOTHING
 LIMIT 100000;
